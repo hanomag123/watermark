e35c6474ba8628f23c18d4f43d0dfd62890f50a0
Zhenya <zhenyaglibko@gmail.com>
2023-09-05 08:57:26 +0300

    last-test

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-// imgInp.onchange = evt => {
-//   const [file] = imgInp.files;
-//   const img = document.getElementById('blah');
-//   if (file && img) {
-//     img.src = URL.createObjectURL(file);
-//     img.classList.add('active')
-//   }
-// }
 
+const saveButton = document.getElementById('saveButton');
+let img = null;
+let bg = null;
 // hiding the div that will contain the images
 const imagesDiv = document.querySelector("#images");
 
 const fileInput = document.querySelector("#upload");
+
+imgInp.onchange = evt => {
+  const [file] = imgInp.files;
+  const img = document.getElementById('blah');
+  if (file && img) {
+    img.src = URL.createObjectURL(file);
+    img.classList.add('active')
+    createWatermarks(img.src)
+  }
+  
+}
 
 fileInput.addEventListener("change", async (e) => {
   const [file] = fileInput.files;
@@ -30,10 +33,15 @@ fileInput.addEventListener("change", async (e) => {
 
 
   originalImage.addEventListener("load", async () => {
-    watermakedImage.src = await watermakImage(
-      originalImage,
-      "./images/1.jpg"
-    );
+    // watermakedImage.src = await watermakImage(
+    //   originalImage,
+    //   "./images/1.jpg"
+    // );
+    bg = originalImage;
+    let realWidth = bg.naturalWidth;
+    let realHeight = bg.naturalHeight;
+    
+    drawImages(img, originalImage)
   });
 
   // making the div containing the image visible
@@ -41,6 +49,8 @@ fileInput.addEventListener("change", async (e) => {
 
   return false;
 });
+
+
 
 function fileToDataUri(field) {
   return new Promise((resolve) => {
@@ -91,7 +101,6 @@ const canvasH = canvas.height = canvas.getBoundingClientRect().height;
 
 canvas.style.border = '1px solid green';
 
-let img = null;
 let watermarks = [];
 let images = [];
 let currentimageIndex = null;
@@ -102,21 +111,20 @@ let startY;
 
 
 
-images.push({ x: 0, y: 0, width: 200, height: 200, color: 'blue' })
 images.push({ x: 200, y: 100, width: 400, height: 200, color: 'red' })
 
 let image = null;
 
 let createWatermarks = async function (src) {
   // loading the watermark image and transforming it into a pattern
-  const result = await fetch('/images/1.jpg');
+  const result = await fetch(src);
   const blob = await result.blob();
   img = await createImageBitmap(blob);
   // pattern = await ctx.createPattern(image, "no-repeat")
-  await drawImages(img)
+  await drawImages(img, bg)
 }
 
-createWatermarks()
+createWatermarks('/images/1.jpg')
 
 let isMouseInimage = function (x, y, image) {
   let image_left = image.x;
@@ -180,7 +188,7 @@ let mouseMove = function (event) {
     currentimage.x += dx;
     currentimage.y += dy;
 
-    drawImages(img);
+    drawImages(img, bg);
 
     startX = mouseX;
     startY = mouseY;
@@ -192,19 +200,64 @@ canvas.onmouseup = mouseUp;
 canvas.onmouseout = mouseOut;
 canvas.onmousemove = mouseMove;
 
+let imgOpacity = 1;
+let scale = 1;
+let drawImages = async function (img = null, bg = null) {
 
-let drawImages = async function (img) {
   ctx.clearRect(0, 0, canvasW, canvasH)
+  if (bg) {
+    ctx.drawImage(bg, 0, 0, canvas.width, canvas.height)
+  }
 
+  ctx.globalAlpha = imgOpacity;
   for (let image of images) {
     ctx.fillStyle = image.color;
     ctx.fillRect(image.x, image.y, image.width, image.height);
     // translating the watermark image to the bottom right corner
-    ctx.drawImage(img, image.x, image.y);
+    if (img) {
+      ctx.drawImage(img, image.x, image.y, image.width, image.height);
+    }
 
     ctx.fill();
 
   }
+  ctx.globalAlpha = 1;
+
 }
+
+if (saveButton) {
+  saveButton.addEventListener('click', function () {
+    this.nextElementSibling.href = canvas.toDataURL()
+    this.nextElementSibling.click()
+  })
+}
+
+
+const inputs = document.querySelectorAll('input[type=range]');
+
+if (inputs.length) {
+  inputs.forEach(range => {
+    range.addEventListener('change', function () {
+      this.nextElementSibling.innerHTML = range.value;
+
+      if (this.id === 'opacity') {
+        imgOpacity = range.value;
+        drawImages(img, bg)
+      }
+
+      if (this.id === 'scale') {
+        scale = range.value;
+        for (let image of images) {
+          image.width = image.width * scale;
+          image.height = image.height * scale;
+        }
+        drawImages(img, bg)
+      }
+    })
+
+    range.nextElementSibling.innerHTML = range.value
+  })
+}
+
 
 
